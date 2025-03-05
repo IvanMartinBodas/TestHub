3762505e73b85dd604689b6971055212f3629bc2
dam1 <imartin@elpuig.xeill.net>
2025-03-05 19:38:11 +0100

    S'han creat 4 metodes nous.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        method2();
+        method3();
+        method4();
+
+    }
+
+    private static void method1() {
+
+    }
+
+    private static void method2() {
+
+    }
+
+    private static void method3() {
+
+    }
+
+    private static void method4() {
+
+    }
+
+
+
+}
